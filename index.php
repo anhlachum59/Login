613c6eb0019e84bbcbaa6e4b0db6483e8c288d12
yselayun <yselayun@gmail.com>
2021-04-03 10:45:01 +0700

    them file css va chinh sua index

--- index.php
+++ index.php
@@ -4,24 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="main_contend">
 <h1>Dang nhap</h1>
-<form action="./DangNhapController.php?action=dangnhap" method="post">
-    
-    <!-- user -->
-    <label>userName: </label><input type="text" name="tennguoidung">
-    
-    <!-- pass -->
+<form class="form_login" action="./DangNhapController.php?action=dangnhap" method="post">
+    <div class="user">
+         <!-- user -->
+        <label>userName: </label><input type="text" name="tennguoidung">
+    </div>
    
-    <label>passWord: </label><input type="password" name="matkhau">
+    
+    <div class="pass">
+        <!-- pass -->
+        <label>passWord: </label><input type="password" name="matkhau">
+    </div>
+    
+   
     
     <!-- login -->
     <input type="submit" value="Dang nhap">
-
-    //aishdauhsdiuas
-    <input type="submit" value="Dang nhap"> <!---->
 </form>
+</div>
+
 </body>
 </html>
