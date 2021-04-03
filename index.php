21ee8c83ef15fc5b5ed49a5cd2e0e459b914f69a
yselayun <yselayun@gmail.com>
2021-04-03 08:31:52 +0700

    Sel them label

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     
     <!-- pass -->
    
-    passWord: <input type="password" name="password">
+    <label>passWord: </label><input type="password" name="password">
     
     <!-- login -->
  
